8346472640fd7cf38a1158928c26b33c08e5223f
dididi <dfdgsdfg@gmail.com>
2015-03-06 18:14:29 +0900

    add parallels-boot2docker ssh link function

--- home/.config/fish/boot2docker.fish
+++
@@ -1,5 +0,0 @@
-# Docker Parallels
-# https://vagrantcloud.com/parallels/boxes/boot2docker
-set -x DOCKER_HOST_IP (vagrant ssh-config | sed -n "s/[ ]*HostName[ ]*//gp")
-set -x DOCKER_HOST tcp://$DOCKER_HOST_IP:2375
-
