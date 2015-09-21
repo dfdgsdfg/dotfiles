8dc69d4167213e842b850788f362a9f905eb4e38
dididi <dfdgsdfg@gmail.com>
2015-09-21 18:05:44 +0900

    update omf init

--- home/.config/omf/init.fish
+++ home/.config/omf/init.fish
@@ -1,6 +1,6 @@
 # Virtualenv
 set -x PIP_REQUIRE_VIRTUALENV true
-set -x PIP_VIRTUALENV_BASE "$HOME/Virtualenvs"
+set -x PIP_VIRTUALENV_BASE "$HOME/.virtualenvs"
 set -x PIP_DOWNLOAD_CACHE "$HOME/.pip/cache"
 
 # PATH
@@ -8,6 +8,6 @@ set -x PATH "$HOME/bin" $PATH
 set -x PATH $PATH "/usr/local/opt/go/libexec/bin"
 set -x PATH $PATH "$HOME/go/bin"
 set -x GOPATH "$HOME/go"
-set -x EDITOR "/usr/local/bin/vim"
+set -x EDITOR "/usr/local/bin/nvim"
 
 source "$HOME/.homesick/repos/homeshick/homeshick.fish"
