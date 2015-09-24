91c4405702ba2f7cea240ea6425eeef64d19742c
dididi <dfdgsdfg@gmail.com>
2015-09-24 11:27:40 +0900

    add pyenv, pyenv-virtualenv

--- home/.config/omf/init.fish
+++ home/.config/omf/init.fish
@@ -1,14 +1,15 @@
-# Virtualenv
-set -x PIP_REQUIRE_VIRTUALENV true
-set -x PIP_VIRTUALENV_BASE "$HOME/.virtualenvs"
-set -x PIP_DOWNLOAD_CACHE "$HOME/.pip/cache"
-
 # PATH
 set -x PATH "$HOME/bin" $PATH
 set -x PATH $PATH "/usr/local/opt/go/libexec/bin"
 set -x PATH $PATH "$HOME/go/bin"
 set -x GOPATH "$HOME/go"
+
+# ENV
 set -x EDITOR "/usr/local/bin/nvim"
 
+# pyenv and pyenv-virtualenv
 status --is-interactive; and . (pyenv init -|psub)
+status --is-interactive; and . (pyenv virtualenv-init -|psub)
+
+# homeshick
 source "$HOME/.homesick/repos/homeshick/homeshick.fish"
