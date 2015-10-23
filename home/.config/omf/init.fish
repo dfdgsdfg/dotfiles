ef2165b76bdf202d501d0a98b5088d04be585aa7
dididi <dfdgsdfg@gmail.com>
2015-10-23 11:23:11 +0900

    update for perlbrew

--- home/.config/omf/init.fish
+++ home/.config/omf/init.fish
@@ -2,7 +2,6 @@
 set -x PATH "$HOME/bin" $PATH
 set -x PATH $PATH "/usr/local/opt/go/libexec/bin"
 set -x PATH $PATH "$HOME/go/bin"
-set -x GOPATH "$HOME/go"
 
 # ENV
 set -x EDITOR "/usr/local/bin/nvim"
@@ -11,5 +10,9 @@ set -x EDITOR "/usr/local/bin/nvim"
 status --is-interactive; and . (pyenv init -|psub)
 status --is-interactive; and . (pyenv virtualenv-init -|psub)
 
+# perlbrew
+. ~/perl5/perlbrew/etc/perlbrew.fish
+
 # homeshick
 source "$HOME/.homesick/repos/homeshick/homeshick.fish"
+
