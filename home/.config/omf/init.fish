9ce44985666b71bddfbd9f873a883c11556091f4
dididi <dfdgsdfg@gmail.com>
2016-01-26 14:22:48 +0900

    update miniconda, icon font

--- home/.config/omf/init.fish
+++ home/.config/omf/init.fish
@@ -2,12 +2,13 @@
 set -x PATH "$HOME/bin" $PATH
 set -x PATH $PATH "/usr/local/opt/go/libexec/bin"
 set -x PATH $PATH "$HOME/go/bin"
+set -x PATH $PATH "$HOME/miniconda3/bin"
 
 # ENV
 set -x EDITOR "/usr/local/bin/nvim"
 
 # vim-devicons
-set -x guifont="Droid Sans Mono for Powerline Nerd Font Complete"
+set -x guifont "Droid Sans Mono for Powerline Nerd Font Complete"
 
 # pyenv and pyenv-virtualenv
 status --is-interactive; and . (pyenv init -|psub)
