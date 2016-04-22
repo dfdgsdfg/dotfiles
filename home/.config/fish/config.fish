5bbbb7387ad714d4d63b48830befdabcca0b8843
dididi <dfdgsdfg@gmail.com>
2016-04-22 23:08:13 +0900

    update config

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,18 +1,19 @@
 # fisher
 set fisher_home ~/.local/share/fisherman
 set fisher_config ~/.config/fisherman
-source $fisher_home/config.fish
 
 # ENV   
 set -x EDITOR "/usr/local/bin/nvim"   
 set -x GOPATH "$HOME/go"    
 set -x GOROOT "/usr/local/opt/go/libexec"
+set -x COMPOSER_HOME "$HOME/.composer"
 
 # PATH   
 set -x PATH "/usr/local/sbin" $PATH
 set -x PATH "$HOME/bin" $PATH   
 set -x PATH $PATH "$GOROOT/bin"
 set -x PATH $PATH "$GOPATH/bin"    
+set -x PATH $PATH "$COMPOSER_HOME/vendor/bin"
 set -x PATH $PATH "$HOME/miniconda3/bin"    
 
 # vim-devicons    
