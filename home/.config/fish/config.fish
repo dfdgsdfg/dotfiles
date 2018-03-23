90117b2843a2559ff47d4acb60a1e0af76162633
dididi <dfdgsdfg@gmail.com>
2018-03-23 18:04:15 +0900

    feat: nvim-complete

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -8,19 +8,23 @@ set -x EDITOR "/usr/local/bin/nvim"
 set -x GOPATH "$HOME/go"    
 set -x GOROOT "/usr/local/opt/go/libexec"
 set -x COMPOSER_HOME "$HOME/.composer"
+set -x HOMEBREW_GITHUB_API_TOKEN "***REMOVED***"
+set -x KUBECONFIG "/Users/dididi/.bluemix/plugins/container-service/clusters/mycluster/kube-config-mel01-mycluster.yml"
 #set -x RBENV_ROOT "/usr/local/var/rbenv"
 #set -x PYENV_ROOT "/usr/local/var/pyenv"
 
 # PATH   
-set -x PATH "/usr/local/sbin" $PATH
 set -x PATH "$HOME/bin" $PATH
 set -x PATH "$HOME/.local/bin" $PATH
-set -x PATH "$HOME/.cabal/bin" $PATH
+set -x PATH "/usr/local/sbin" $PATH
+set -x PATH $PATH "$HOME/.cabal/bin"
+set -x PATH $PATH "$HOME/.cargo/bin" 
 set -x PATH $PATH "$GOROOT/bin"
-set -x PATH $PATH "$GOPATH/bin"    
+set -x PATH $PATH "$GOPATH/bin"
 set -x PATH $PATH "$COMPOSER_HOME/vendor/bin"
 set -x PATH $PATH "/usr/local/share/dotnet"
 set -x PATH $PATH "$HOME/opt/google-cloud-sdk/bin"
+set -x PATH $PATH "$HOME/.fastlane/bin"
     
 # pyenv and pyenv-virtualenv    
 status --is-interactive; and . (pyenv init -|psub)    
@@ -30,9 +34,15 @@ status --is-interactive; and . (pyenv virtualenv-init -|psub)
 status --is-interactive; and . (rbenv init -|psub)
 
 # eval    
+eval (direnv hook fish)
 source "$HOME/perl5/perlbrew/etc/perlbrew.fish"
 source "$HOME/.homesick/repos/homeshick/homeshick.fish"
+source /usr/local/opt/asdf/asdf.fish
 
 # iterm
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
+
+# tabtab source for electron-forge package
+# uninstall by removing these lines or running `tabtab uninstall electron-forge`
+[ -f /usr/local/lib/node_modules/electron-forge/node_modules/tabtab/.completions/electron-forge.fish ]; and . /usr/local/lib/node_modules/electron-forge/node_modules/tabtab/.completions/electron-forge.fish
