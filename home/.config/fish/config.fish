d506d653a1ec4c39dbe52b8895f655acd7e35dd2
dididi <dfdgsdfg@gmail.com>
2021-10-30 21:10:39 +0900

    chore: chore

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -16,42 +16,41 @@ set -Ua fish_user_paths ~/.cabal/bin
 set -Ua fish_user_paths ~/.cargo/bin
 set -Ua fish_user_paths ~/.pub-cache/bin
 set -Ua fish_user_paths ~/flutter/bin
-set -Ua fish_user_paths $GOROOT/bin
-set -Ua fish_user_paths $GOPATH/bin
 set -Ua fish_user_paths $COMPOSER_HOME/vendor/bin
 set -Ua fish_user_paths /usr/local/share/dotnet
 set -Ua fish_user_paths ~/opt/google-cloud-sdk/bin
 set -Ua fish_user_paths ~/Library/Android/sdk/platform-tools
 
 
-# eval    
-source ~/.config/fish/credential.fish
-source ~/.homesick/repos/homeshick/homeshick.fish
-source /usr/local/opt/asdf/libexec/asdf.fish
-# source (brew --prefix asdf)/asdf.fish
-# source /opt/homebrew/opt/asdf/libexec/asdf.fish
-# source (/opt/homebrew/bin/brew --prefix asdf)/asdf.fish
-eval (direnv hook fish)
-navi widget fish | source
-zoxide init fish | source
-## asdf-java
-asdf current java 2>&1 > /dev/null
-if test $status -eq 0
-    set -x JAVA_HOME (asdf where java)
-end
-
-
 # ENV   
 set -x LANG "ko_KR.UTF-8"
 set -x EDITOR /usr/local/bin/nvim
-set -x GOPATH ~/go    
-set -x GOROOT (asdf where golang)/go
 set -x COMPOSER_HOME ~/.composer
 set -x GO111MODULE "on"
 set -x CLOUDSDK_PYTHON_SITEPACKAGES "1"
 # flutter
 # https://github.com/flutter/flutter/issues/25112
 set -gx NO_PROXY localhost,127.0.0.1
+
+
+# eval    
+test -e ~/.iterm2_shell_integration.fish ; and source ~/.iterm2_shell_integration.fish
+source ~/.config/fish/credential.fish
+source ~/.homesick/repos/homeshick/homeshick.fish
+navi widget fish | source
+zoxide init fish | source
+starship init fish | source
+
+
+# asdf
+source /usr/local/opt/asdf/libexec/asdf.fish
+# source (brew --prefix asdf)/asdf.fish
+# source /opt/homebrew/opt/asdf/libexec/asdf.fish
+# source (/opt/homebrew/bin/brew --prefix asdf)/asdf.fish
+eval (direnv hook fish)
+## asdf-java
+asdf current java 2>&1 > /dev/null
+. ~/.asdf/plugins/java/set-java-home.fish
 
 
 # alias
@@ -62,12 +61,5 @@ alias lla="ls -la"
 alias lt="ls --tree"
 alias cat="bat"
 alias cd="z"
-
-
-# iterm
-test -e ~/.iterm2_shell_integration.fish ; and source ~/.iterm2_shell_integration.fish
-
-
-# starship
-starship init fish | source
+alias gcloud="env ASDF_PYTHON_VERSION=3.7.12 gcloud"
 
