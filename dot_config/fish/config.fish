fe5ebdda755fb5e3d77faaacbbc71ff5a07b3b80
sg <sg@us-all.co.kr>
2024-08-13 16:15:41 +0900

    feat: flutter migrate to mise

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -47,8 +47,6 @@ set -Up fish_user_paths ~/opt/bin
 set -Up fish_user_paths ~/.local/bin
 set -Up fish_user_paths ~/bin
 set -Ua fish_user_paths ~/opt/google-cloud-sdk/bin
-set -Ua fish_user_paths ~/.pub-cache/bin
-set -Ua fish_user_paths ~/flutter/bin
 set -Ua fish_user_paths ~/.shorebird/bin
 set -Ua fish_user_paths ~/Library/Android/sdk/tools
 set -Ua fish_user_paths ~/Library/Android/sdk/tools/bin
@@ -72,7 +70,7 @@ set -x NODE_OPTIONS "--max-old-space-size=8096"
 # https://stackoverflow.com/questions/71320584/flutter-build-ios-got-error-requested-but-did-not-find-extension-point-with-ide
 # set -x SDKROOT (xcrun -sdk macosx --show-sdk-path)
 set -x ANDROID_HOME ~/Library/Android/sdk
-
+set -x FLUTTER_ROOT (mise where flutter)
 
 # eval    
 test -e ~/.iterm2_shell_integration.fish; and source ~/.iterm2_shell_integration.fish
